ba6522ef906d82c64427f127ce3983c981173204
森雄大 <ougai.buturi.14142@gmail.com>
2019-11-19 16:45:29 +0900

    commit

--- trans_paper.py
+++ trans_paper.py
@@ -103,5 +103,5 @@ def pipe():
 
 if __name__ == '__main__':
     app.debug = True
-    server = pywsgi.WSGIServer(("", 8000), app, handler_class=WebSocketHandler, log=logger)
+    server = pywsgi.WSGIServer(("", 8000), app, handler_class=WebSocketHandler, log=logger, error_log=logger)
     server.serve_forever()
